63e181b8519992f78c6fe773e631fdb6271a7fc2
ravine13 <kuriaderek@gmail.com>
2023-10-27 20:12:45 +0300

    completed Assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      <button
+        className={selectedCategory === "All" ? "selected" : ""}
+        onClick={() => onSelectCategory("All")}
+      >
+        All
+      </button>
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={selectedCategory === category ? "selected" : ""}
+          onClick={() => onSelectCategory(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task({ task, onDelete }) {
+  const { text, category } = task;
+
+  const handleDelete = () => {
+    onDelete(task.id);
+  };
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
